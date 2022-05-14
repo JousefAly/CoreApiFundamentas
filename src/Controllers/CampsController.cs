664c973a6a61836c694868d2877d523db9b740de
JousefAly <youssefali00286@gmail.com>
2022-05-14 11:08:35 +0200

    Created api/camps

---
+++ src/Controllers/CampsController.cs
@@ -0,0 +1,36 @@
+﻿using CoreCodeCamp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[Controller]")]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository _campRepository;
+
+        public CampsController(ICampRepository campRepository)
+        {
+            _campRepository = campRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCamps()
+        {
+            try
+            {
+                var result = await _campRepository.GetAllCampsAsync();
+                return Ok(result);
+            }
+            catch(Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+    }
+}
